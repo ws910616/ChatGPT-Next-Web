01beb25e9984bfb26d87ed9a9c3373b530ce052f
ws910616 <105333186+ws910616@users.noreply.github.com>
2024-07-26 22:30:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "伦伟实业ChatGPT",
+  description: "伦伟实业AI智能助手.",
   appleWebApp: {
-    title: "NextChat",
+    title: "伦伟实业ChatGPT",
     statusBarStyle: "default",
   },
 };
